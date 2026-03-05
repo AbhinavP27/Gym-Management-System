d94d6c98489fb71d3d22e47931e2435848e071df
AbhinavP <apkunju04@gmail.com>
2026-03-05 22:21:02 +0530

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import './style/About.css'
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='about'> 
+      <div className='box '>
+        <h1>Urban Grind Fitness</h1>
+        <p>
+          Urban Grind Fitness is dedicated to helping individuals achieve their fitness goals through professional training, modern equipment, and a supportive community. Our mission is to promote strength, health, and confidence by creating an environment where everyone—from beginners to athletes—can grow and succeed.
+        </p>
+      </div>
+    </section>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './style/Navbar.css'
+// import logo from '../assets/logo-1.png'
 
 const Navbar = () => {
   return (
     <div className='navbar shadow-lg'>
-      <a className="navbar-brand fw-bold text-danger p-2" href="#">
-            Urban<span className="b-4 " style={{ color: '#0af4e5' }}>Grind</span>
+      <a className="navbar-brand fw-bold text-white p-2" href="#">
+            URBAN<span className="b-4 " style={{ color: '#c9f40a' }}>GRIND</span>
           </a>
+
+      {/* <img className='urban' src={logo} alt="Logo" />   */}
 
       <ul className='nav-links m-1'>
         <li><a href="#">Home</a></li>
@@ -16,7 +19,7 @@ const Navbar = () => {
         <li><a href="#">Plans</a></li>
         <li><a href="#">Testimonials</a></li>
         <li><a href="#">Contact</a></li>
-        <li><a href="#">Login</a></li>
+        <li><button className="join-btn">Login</button></li>
         <li><button className="join-btn">Join Now</button></li>
       </ul>
     </div>
